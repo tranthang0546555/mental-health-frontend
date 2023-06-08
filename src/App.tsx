6011124e01b11d41e29ed345662f99fcc6c134f8
airnang <baovd@meta-technology.com>
2023-06-08 22:24:41 +0700

    login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header/Header";
@@ -8,56 +13,28 @@ import Contact from "./pages/contact";
 import FAQ from "./pages/faq";
 import HomePage from "./pages/home";
 import News from "./pages/news";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />,
-      id: "Home",
-    },
-    {
-      path: "/contact",
-      element: (
-        <InnerPage title="Contact">
-          <Contact />
-        </InnerPage>
-      ),
-      id: "Contact",
-    },
-    {
-      path: "/faq",
-      element: (
-        <InnerPage title="FAQ">
-          <FAQ />
-        </InnerPage>
-      ),
-    },
-    {
-      path: "/about",
-      element: (
-        <InnerPage title="About">
-          <About />
-        </InnerPage>
-      ),
-    },
-    {
-      path: "/news",
-      element: (
-        <InnerPage title="News">
-          <News />
-        </InnerPage>
-      ),
-    },
-  ]);
-  console.log("router", router);
-  return (
-    <>
-      <Header />
-      <RouterProvider router={router} />
-      <Footer />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<HomePage />}>
+          <Header />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<HomePage />} />
+          <Footer />
+        </Route>
+      </>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,90 @@
+export default function Login() {
+  return (
+    <section className="section register d-flex flex-column align-items-center justify-content-center py-4">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+            <div className="d-flex justify-content-center py-4"></div>
+            <div className="card mb-3">
+              <div className="card-body">
+                <div className="pt-4 pb-2">
+                  <h5 className="card-title text-center pb-0 fs-4">
+                    Login to Your Account
+                  </h5>
+                  <p className="text-center small">
+                    Enter your username & password to login
+                  </p>
+                </div>
+
+                <form className="row g-3 needs-validation" noValidate>
+                  <div className="col-12">
+                    <label htmlFor="yourEmail" className="form-label">
+                      Email
+                    </label>
+                    <div className="input-group has-validation">
+                      <span className="input-group-text" id="inputGroupPrepend">
+                        @
+                      </span>
+                      <input
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        id="yourEmail"
+                        required
+                      />
+                      <div className="invalid-feedback">
+                        Please enter your username.
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="col-12">
+                    <label htmlFor="yourPassword" className="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      id="yourPassword"
+                      required
+                    />
+                    <div className="invalid-feedback">
+                      Please enter your password!
+                    </div>
+                  </div>
+
+                  <div className="col-12">
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        name="remember"
+                        value="true"
+                        id="rememberMe"
+                      />
+                      <label className="form-check-label" htmlFor="rememberMe">
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <button className="btn btn-primary w-100" type="submit">
+                      Login
+                    </button>
+                  </div>
+                  <div className="col-12">
+                    <p className="small mb-0">
+                      Don't have account?{" "}
+                      <a href="/register">Create an account</a>
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
