ec9af1041d7b04be4e4d4c6b5d95ad6f6935b359
Thang Tran <thangtn@meta-technology.com.vn>
2023-06-10 09:33:16 +0700

    remove dispatch

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -4,4 +4,3 @@ import type { RootState, AppDispatch } from "../store";
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const dispatch = useAppDispatch();
