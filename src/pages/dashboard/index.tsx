5ef31014f60eea22128688511e26d2cb91b70ef9
airnang <baovd@meta-technology.com>
2023-06-13 10:03:36 +0700

    test

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,22 +1,10 @@
-import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import HeaderDashboard from "../../components/Dashboard/Header";
 import SidebarDashboard from "../../components/Dashboard/Sidebar";
 import { useAppSelector } from "../../hooks/store";
-import { useDebouncedCallback } from "use-debounce";
 
 export default function Dashboard() {
   const { login } = useAppSelector((state) => state.auth);
-  const navigate = useNavigate();
-
-  const check = useDebouncedCallback(() => {
-    if (!login) navigate("/login");
-    // else navigate("/dashboard");
-  }, 3000);
-
-  useEffect(() => {
-    !login && check();
-  }, [login]);
 
   if (!login) return <></>;
   return (
