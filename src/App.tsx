d217d0dbf0193e3de31c440dc962397eee5d650d
airnang <baovd@meta-technology.com>
2023-06-14 09:58:51 +0700

    lazy load and config chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
+import { lazy } from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
-import Header from "./components/Header/Header";
+import Header from "./components/Header";
 import InnerPage from "./components/InnerPage";
 import { preLoadPage } from "./hooks";
-import About from "./pages/about";
-import Login from "./pages/auth/login";
-import Register from "./pages/auth/register";
-import Contact from "./pages/contact";
-import Dashboard from "./pages/dashboard";
-import UserList from "./pages/dashboard/admin/user-manager/UserList";
-import LockUserList from "./pages/dashboard/admin/user-manager/UserLockList";
-import BlankPage from "./pages/dashboard/blank";
-import PostCreate from "./pages/dashboard/doctor/post-manager/PostCreate";
-import PostEdit from "./pages/dashboard/doctor/post-manager/PostEdit";
-import PostList from "./pages/dashboard/doctor/post-manager/PostList";
-import Profile from "./pages/dashboard/profile";
-import Doctor from "./pages/doctor";
-import FAQ from "./pages/faq";
-import HomePage from "./pages/home";
-import Post from "./pages/post";
-import PostDetail from "./pages/post-detail";
-import Appointment from "./pages/appointment";
+
+const HomePage = lazy(() => import("./pages/home"));
+const Post = lazy(() => import("./pages/post"));
+const FAQ = lazy(() => import("./pages/faq"));
+const Doctor = lazy(() => import("./pages/doctor"));
+const PostDetail = lazy(() => import("./pages/post-detail"));
+const Profile = lazy(() => import("./pages/dashboard/profile"));
+const PostList = lazy(
+  () => import("./pages/dashboard/doctor/post-manager/PostList")
+);
+const PostEdit = lazy(
+  () => import("./pages/dashboard/doctor/post-manager/PostEdit")
+);
+const PostCreate = lazy(
+  () => import("./pages/dashboard/doctor/post-manager/PostCreate")
+);
+const LockUserList = lazy(
+  () => import("./pages/dashboard/admin/user-manager/UserLockList")
+);
+const UserList = lazy(
+  () => import("./pages/dashboard/admin/user-manager/UserList")
+);
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const BlankPage = lazy(() => import("./pages/dashboard/blank"));
+const Contact = lazy(() => import("./pages/contact"));
+const Register = lazy(() => import("./pages/auth/register"));
+const Login = lazy(() => import("./pages/auth/login"));
+const Appointment = lazy(() => import("./pages/appointment"));
+const About = lazy(() => import("./pages/about"));
 
 function App() {
   preLoadPage();
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": [
+            "react",
+            "react-dom",
+            "react-router-dom",
+            "react-toastify",
+          ],
+          "@reduxjs": ["@reduxjs/toolkit"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1200,
+  },
+});
