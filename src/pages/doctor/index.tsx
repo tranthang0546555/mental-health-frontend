6d4fd33938e8ccc784b211e82e643c79941566c0
thangne <tranthang0546555@gmail.com>
2023-09-03 23:41:42 +0700

    update skeleton

--- src/pages/doctor/index.tsx
+++ src/pages/doctor/index.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "react-router-dom";
 import { DOCTOR_LIST, useApi } from "../../api";
 import Pagination from "../../components/Pagination";
 import Search from "../../components/Search";
+import Skeleton from "../../components/Skeleton";
 import DoctorItem from "./DoctorItem";
 import "./index.css";
 
@@ -15,7 +16,7 @@ export default function Doctor() {
   useEffect(() => {
     const searchParamsObject = Object.fromEntries(searchParams);
     setFilters(searchParamsObject);
-  }, []);
+  }, [searchParams]);
 
   const fetchData = async (queries?: { [key: string]: unknown }) => {
     queries &&
@@ -31,7 +32,7 @@ export default function Doctor() {
     return data;
   };
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["doctors", filters],
     queryFn: () => fetchData(filters),
   });
@@ -42,16 +43,40 @@ export default function Doctor() {
         <div className="row g-5">
           <div className="col-lg-9">
             <div className="row gy-4 posts-list">
-              {data?.data.map((doctor, idx) => (
-                <div className="col-lg-6" key={idx}>
-                  <DoctorItem data={doctor} />
-                </div>
-              ))}
+              {isLoading ? (
+                Array.from(new Array(6)).map((_, idx) => (
+                  <div className="col-lg-6 row p-3" key={idx}>
+                    <div className="col d-flex align-items-center">
+                      <Skeleton
+                        variant="circular"
+                        height="120px"
+                        width="120px"
+                      />
+                    </div>
+                    <div className="col-8">
+                      <Skeleton variant="rounded" />
+                      <Skeleton variant="text" />
+                      <Skeleton variant="rounded" height="120px" />
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <>
+                  {data?.data.map((doctor, idx) => (
+                    <div className="col-lg-6" key={idx}>
+                      <DoctorItem data={doctor} />
+                    </div>
+                  ))}
+                  <Pagination
+                    pagination={data as Pagination}
+                    onChange={(page) =>
+                      setFilters((prev) => ({ ...prev, page }))
+                    }
+                  />
+                </>
+              )}
+              {data?.totalRecords === 0 && <h4>Không tìm thấy kết quả nào!</h4>}
             </div>
-            <Pagination
-              pagination={data as Pagination}
-              onChange={(page) => setFilters((prev) => ({ ...prev, page }))}
-            />
           </div>
 
           <div className="col-lg-3">
