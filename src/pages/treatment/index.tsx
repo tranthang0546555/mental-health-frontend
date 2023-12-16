72581f67f35be151534438d60e049d12c8db68b4
thangne <tranthang0546555@gmail.com>
2023-12-17 00:54:35 +0700

    clean code

--- src/pages/treatment/index.tsx
+++ src/pages/treatment/index.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { TREATMENT_LIST, useApi } from "../../api";
+import Skeleton from "../../components/Skeleton";
 import { audioPath, videoPath } from "../../utils";
 import "./index.css";
 
@@ -19,33 +20,47 @@ export default function Treatment() {
     <section id="treatment" className="treatment">
       <div className="container">
         <div className="row">
-          {data?.data.map((item) => {
-            return (
-              <div className="col-4 p-3 " key={item._id}>
-                <div className="card">
-                  <div className="preview">
-                    <video
-                      src={
-                        item.type === "video"
-                          ? videoPath(item.file)
-                          : audioPath(item.file)
-                      }
-                      className="card-img-top"
-                      controls
-                    ></video>
-                  </div>
-
-                  <div className="card-body">
-                    <h5 className="card-title">{item.title}</h5>
-                    <p className="card-text">{item.description}</p>
-                    <a href="#" className="btn btn-primary">
-                      Phát ngay
-                    </a>
-                  </div>
-                </div>
+          {isLoading ? (
+            Array.from(new Array(6)).map((_, idx) => (
+              <div className="col-lg-6" key={idx}>
+                <Skeleton variant="rounded" height="120px" />
+                <Skeleton variant="rounded" />
+                <Skeleton variant="text" />
+                <Skeleton variant="rounded" />
               </div>
-            );
-          })}
+            ))
+          ) : (
+            <>
+              {data?.data.map((item) => {
+                return (
+                  <div className="col-4 p-3 " key={item._id}>
+                    <div className="card">
+                      <div className="preview">
+                        <video
+                          src={
+                            item.type === "video"
+                              ? videoPath(item.file)
+                              : audioPath(item.file)
+                          }
+                          className="card-img-top"
+                          controls
+                        ></video>
+                      </div>
+
+                      <div className="card-body">
+                        <h5 className="card-title">{item.title}</h5>
+                        <p className="card-text">{item.description}</p>
+                        <a href="#" className="btn btn-primary">
+                          Phát ngay
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          )}
+          {data?.totalRecords === 0 && <h4>Không tìm thấy kết quả nào!</h4>}
         </div>
       </div>
     </section>
